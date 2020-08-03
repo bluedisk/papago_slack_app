47dc7dc256c2c1bddb12eecb37bd606b21220252
bluedisk <bluedisk@gmail.com>
2020-08-03 23:33:00 +0900

    unit test for new block types

--- papago_slack/tests/tests_parser.py
+++ papago_slack/tests/tests_parser.py
@@ -36,6 +36,15 @@ BLOCKS1 = """
               {
                 "name": "smirk",
                 "type": "emoji"
+              },
+              {
+                "channel_id": "C0166CJGQ2F",
+                "type": "channel"
+              },
+              {
+                "range": "here",
+                "style": {"bold": true},    
+                "type": "broadcast"
               }
             ],
             "type": "rich_text_section"
@@ -49,11 +58,17 @@ BLOCKS1 = """
 class ParserTestCase(TestCase):
     def test_sanitizing(self):
         extra_list, text = papago.sanitize(json.loads(BLOCKS1))
-        self.assertEqual(text, "파파고 [1]  바보 [2] 다람쥐 [3]")
-        self.assertListEqual(extra_list, [('user', 'UFGQX1QFK'), ('link', "https://google.com"), ('emoji', 'smirk')])
+        self.assertEqual(text, "파파고 [1]  바보 [2] 다람쥐 [3][4][5]")
+        self.assertListEqual(extra_list, [
+            ('user', 'UFGQX1QFK'),
+            ('link', "https://google.com"),
+            ('emoji', 'smirk'),
+            ('channel', 'C0166CJGQ2F'),
+            ('broadcast', 'here')
+        ])
 
         text = papago.desanitize(extra_list, text)
-        self.assertEqual(text, "파파고 <@UFGQX1QFK>  바보 <https://google.com> 다람쥐 :smirk:")
+        self.assertEqual(text, "파파고 <@UFGQX1QFK>  바보 <https://google.com> 다람쥐 :smirk:<#C0166CJGQ2F><!here>")
 
     def text_language_detection(self):
         from_lang, to_lang = papago.recognize_language('파파고\xa0<@UFGQX1QFK>\xa0\xa0바보\xa0<https://google.com>\xa0다람쥐\xa0:smirk:')
