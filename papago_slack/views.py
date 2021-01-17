7efd5d8c5f5504d6f3697a8a96e2f6f8f35b5a82
bluedisk <bluedisk@gmail.com>
2021-01-17 21:43:36 +0900

    change landing & auth completed page to using template that looks much better

--- papago_slack/views.py
+++ papago_slack/views.py
@@ -1,9 +1,6 @@
 from django.conf import settings
-from django.http import HttpResponse
-from django.urls import reverse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse(f"Hi! there? I'm Papago Slack App #{settings.SLACK_CLIENT_ID}<p/>" + 
-                        f"<a href=\"{reverse('install')}\">" +
-                        '<img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"></a>')
+    return render(request, 'landing.html', {'slack_client_id': settings.SLACK_CLIENT_ID})
